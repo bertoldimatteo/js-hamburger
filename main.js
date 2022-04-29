7f2b27a71856069263dcb810863b9535ee577d2a
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-04-29 15:23:33 +0200

    aggiunte funzioni apertura/chiusura menù

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-const menu = document.getElementsByClassName('hamburger-menu')[0];
-menu.setAttribute('id', 'hamburger-menu');
+const addIdMenu = document.getElementsByClassName('hamburger-menu')[0];
+addIdMenu.setAttribute('id', 'hamburger-menu');
+
+const showMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+
+showMenu.addEventListener('click', 
+    function(){
+        document.getElementById('hamburger-menu').style.display = 'block';
+    }
+);
+
+closeMenu.addEventListener('click', 
+    function(){
+        document.getElementById('hamburger-menu').style.display = 'none';
+    }
+);
